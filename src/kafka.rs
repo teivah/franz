91957986fceb57aaf986134fd72bf61bf19fdf6f
teivah <t.harsanyi@thebeat.co>
2020-06-09 18:33:02 +0200

    Kafka producer

---
+++ src/kafka.rs
@@ -0,0 +1,68 @@
+use kafka::producer::{Producer, Record, RequiredAcks};
+use std::borrow::Borrow;
+use std::fmt::Write;
+use uuid::Uuid;
+
+struct KafkaConfiguration {
+    hosts: Vec<String>,
+    requested_required_acks: i8,
+}
+
+struct TestConfiguration<'a> {
+    topic: &'a str,
+    payload: &'a str,
+}
+
+struct Tester<'a> {
+    campaignID: &'a str,
+    producer: Producer,
+}
+
+impl Tester<'_> {
+    fn new(hosts: Vec<String>, requested_required_acks: i8) -> Result<Self, String> {
+        let required_acks: RequiredAcks;
+        match requested_required_acks {
+            1 => {
+                required_acks = RequiredAcks::One;
+            }
+            0 => {
+                required_acks = RequiredAcks::None;
+            }
+            -1 => {
+                required_acks = RequiredAcks::All;
+            }
+            v => {
+                return Err(format_args!(
+                    "unknown required acks option ({}), it should be either 1, 0 or -1",
+                    requested_required_acks
+                )
+                .to_string())
+            }
+        }
+
+        let result_producer = Producer::from_hosts(hosts)
+            .with_required_acks(required_acks)
+            .create();
+        match result_producer {
+            Err(e) => return Err(format_args!("unable to create producer: {}", e).to_string()),
+            Ok(_) => (),
+        }
+
+        let tester = Tester {
+            campaignID: Uuid::new_v4().to_string().as_str(),
+            producer: result_producer.unwrap(),
+        };
+        Ok(tester)
+    }
+
+    fn produce(&self) {
+        let mut buf = String::with_capacity(2);
+        for i in 0..10 {
+            let _ = write!(&mut buf, "{}", i); // some computation of the message data to be sent
+            self.producer
+                .send(&Record::from_value("my-topic", buf.as_bytes()))
+                .unwrap();
+            buf.clear();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+mod kafka;
+
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use clap::{App as ClapApp, Arg};
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    setup();
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
+            .service(web::resource("/").to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+fn setup() {
+    let matches = ClapApp::new("franz")
+        .arg(
+            Arg::with_name("foo")
+                .short("f")
+                .long("foo")
+                .takes_value(true)
+                .help("help"),
+        )
+        .get_matches();
+
+    let foo = matches.value_of("foo").unwrap();
+    println!("{}", foo)
 }
