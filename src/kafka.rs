71f1ddf6d186ed452bcd914bec4451584e31ace7
teivah <t.harsanyi@thebeat.co>
2020-06-09 19:30:45 +0200

    Kafka producer

--- src/kafka.rs
+++ src/kafka.rs
@@ -3,62 +3,62 @@ use std::borrow::Borrow;
 use std::fmt::Write;
 use uuid::Uuid;
 
-struct KafkaConfiguration {
+struct KafkaCfg {
     hosts: Vec<String>,
     requested_required_acks: i8,
 }
 
-struct TestConfiguration<'a> {
-    topic: &'a str,
-    payload: &'a str,
+struct SendCfg {
+    topic: String,
+    payload: String,
+    number_of_messages: i32,
 }
 
-struct Tester<'a> {
-    campaignID: &'a str,
+struct Tester {
+    campaign_id: Uuid,
     producer: Producer,
+    send_cfg: SendCfg,
 }
 
-impl Tester<'_> {
-    pub fn new(hosts: Vec<String>, requested_required_acks: i8) -> Result<Self, String> {
-        let result = create_producer(hosts, requested_required_acks)?;
-
+impl Tester {
+    pub fn new(kafka_cfg: KafkaCfg, send_cfg: SendCfg) -> Result<Self, String> {
         Ok(Tester {
-            campaignID: Uuid::new_v4().to_string().as_str(),
-            producer: result,
+            campaign_id: Uuid::new_v4(),
+            producer: create_producer(kafka_cfg)?,
+            send_cfg,
         })
     }
 
-    fn produce(&self) {
-        let mut buf = String::with_capacity(2);
-        for i in 0..10 {
-            let _ = write!(&mut buf, "{}", i); // some computation of the message data to be sent
-            self.producer
-                .send(&Record::from_value("my-topic", buf.as_bytes()))
-                .unwrap();
-            buf.clear();
+    fn produce(&mut self) {
+        for i in 0..self.send_cfg.number_of_messages {
+            let result = self.producer.send(&Record::from_value(
+                self.send_cfg.topic.as_str(),
+                self.send_cfg.payload.as_str(),
+            ));
         }
     }
 }
 
-fn create_producer(hosts: Vec<String>, requested_required_acks: i8) -> Result<Producer, String> {
-    let required_acks = match requested_required_acks {
+fn create_producer(kafka_cfg: KafkaCfg) -> Result<Producer, String> {
+    let required_acks = match kafka_cfg.requested_required_acks {
         1 => RequiredAcks::One,
         0 => RequiredAcks::None,
         -1 => RequiredAcks::All,
         _ => {
             return Err(format_args!(
                 "unknown required acks option ({}), it should be either 1, 0 or -1",
-                requested_required_acks
+                kafka_cfg.requested_required_acks
             )
             .to_string())
         }
     };
 
-    let result_producer = Producer::from_hosts(hosts)
+    let producer = Producer::from_hosts(kafka_cfg.hosts)
         .with_required_acks(required_acks)
         .create();
-    match result_producer {
-        Err(e) => return Err(format_args!("unable to create producer: {}", e).to_string()),
-        Ok(p) => return Ok(p),
-    }
+
+    return match producer {
+        Err(e) => Err(format_args!("unable to create producer: {}", e).to_string()),
+        Ok(p) => Ok(p),
+    };
 }
