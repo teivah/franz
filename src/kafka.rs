b0257bad4f3a1bbe6849c445cb18f67fc2de0653
teivah <t.harsanyi@thebeat.co>
2020-06-09 23:33:23 +0200

    Kafka producer

--- src/kafka.rs
+++ src/kafka.rs
@@ -3,33 +3,29 @@ use std::borrow::Borrow;
 use std::fmt::Write;
 use uuid::Uuid;
 
-struct KafkaCfg {
-    hosts: Vec<String>,
-    requested_required_acks: i8,
+pub struct SendCfg {
+    pub topic: String,
+    pub payload: String,
+    pub number_of_messages: i32,
+    pub requested_required_acks: i8,
 }
 
-struct SendCfg {
-    topic: String,
-    payload: String,
-    number_of_messages: i32,
-}
-
-struct Tester {
-    campaign_id: Uuid,
+pub struct Worker {
+    id: Uuid,
     producer: Producer,
     send_cfg: SendCfg,
 }
 
-impl Tester {
-    pub fn new(kafka_cfg: KafkaCfg, send_cfg: SendCfg) -> Result<Self, String> {
-        Ok(Tester {
-            campaign_id: Uuid::new_v4(),
-            producer: create_producer(kafka_cfg)?,
+impl Worker {
+    pub fn new(hosts: Vec<String>, send_cfg: SendCfg) -> Result<Self, String> {
+        Ok(Worker {
+            id: Uuid::new_v4(),
+            producer: create_producer(hosts, send_cfg.requested_required_acks)?,
             send_cfg,
         })
     }
 
-    fn produce(&mut self) {
+    pub async fn produce(&mut self) {
         for i in 0..self.send_cfg.number_of_messages {
             let result = self.producer.send(&Record::from_value(
                 self.send_cfg.topic.as_str(),
@@ -37,23 +33,27 @@ impl Tester {
             ));
         }
     }
+
+    pub fn id(&self) -> String {
+        self.id.to_string()
+    }
 }
 
-fn create_producer(kafka_cfg: KafkaCfg) -> Result<Producer, String> {
-    let required_acks = match kafka_cfg.requested_required_acks {
+fn create_producer(hosts: Vec<String>, requested_required_acks: i8) -> Result<Producer, String> {
+    let required_acks = match requested_required_acks {
         1 => RequiredAcks::One,
         0 => RequiredAcks::None,
         -1 => RequiredAcks::All,
         _ => {
             return Err(format_args!(
                 "unknown required acks option ({}), it should be either 1, 0 or -1",
-                kafka_cfg.requested_required_acks
+                requested_required_acks
             )
             .to_string())
         }
     };
 
-    let producer = Producer::from_hosts(kafka_cfg.hosts)
+    let producer = Producer::from_hosts(hosts)
         .with_required_acks(required_acks)
         .create();
 
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,94 @@
 mod kafka;
 
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
-use clap::{App as ClapApp, Arg};
+use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer};
+use clap::{App as ClapApp, Arg, ArgMatches};
+use serde::{Deserialize, Serialize};
+use std::borrow::Borrow;
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {:?}", req);
-    "Hello world!"
+fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    setup();
+    let args = setupApplication();
+    let kafka_hosts = args
+        .value_of("kafka_hosts")
+        .expect("kafka_hosts argument not set");
 
+    // let server = ;
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+
+    let req = Server::new(kafka_hosts);
+    let f1 = |x: web::Json<Request>| Server::produce(req, x);
 
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
+            .service(web::resource("/produce").route(web::post().to(f1)))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-fn setup() {
-    let matches = ClapApp::new("franz")
+fn setupApplication() -> &'static ArgMatches<'static> {
+    &ClapApp::new("franz")
         .arg(
-            Arg::with_name("foo")
-                .short("f")
-                .long("foo")
+            Arg::with_name("kafka_hosts")
+                .short("k")
+                .long("kafka_hosts")
                 .takes_value(true)
-                .help("help"),
+                .help("Kafka hosts separated by a colon"),
         )
-        .get_matches();
+        .get_matches()
+}
 
-    let foo = matches.value_of("foo").unwrap();
-    println!("{}", foo)
+struct Server {
+    kafka_hosts: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    topic: String,
+    payload: String,
+    number_of_messages: i32,
+    requested_required_acks: i8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    id: String,
+}
+
+impl Server {
+    fn new(kafka_hosts: &str) -> &'static Self {
+        &Server {
+            kafka_hosts: kafka_hosts
+                .split(",")
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    async fn produce(&self, req: web::Json<Request>) -> HttpResponse {
+        let worker_result = kafka::Worker::new(
+            self.kafka_hosts.to_vec(),
+            kafka::SendCfg {
+                topic: (&req.topic).to_string(),
+                payload: (&req.payload).to_string(),
+                number_of_messages: req.number_of_messages,
+                requested_required_acks: req.requested_required_acks,
+            },
+        );
+
+        let mut worker = match worker_result {
+            Err(e) => return HttpResponse::InternalServerError().body(e),
+            Ok(e) => e,
+        };
+
+        let _ = worker.produce();
+        HttpResponse::Ok().json(Response { id: worker.id() })
+    }
 }
